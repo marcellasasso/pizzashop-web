992149ec28eaf5e705a65f5fa566b9451bb79e9b
Marcella <marcellasasso@gmail.com>
2024-11-21 16:03:26 -0300

    páginas e componentes (UI) - até aula página: listagem de pedidos

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <Toaster richColors />
-      <RouterProvider
-        router={router}
-        future={{
-          v7_startTransition: true,
-        }}
-      />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <Toaster richColors />
+        <RouterProvider
+          router={router}
+          future={{
+            v7_startTransition: true,
+          }}
+        />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,7 +3,9 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
+import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router = createBrowserRouter(
   [
@@ -15,9 +17,12 @@ export const router = createBrowserRouter(
           path: '/',
           element: <Dashboard />,
         },
+        {
+          path: '/orders',
+          element: <Orders />,
+        },
       ],
     },
-
     {
       path: '/',
       element: <AuthLayout />,
@@ -25,6 +30,10 @@ export const router = createBrowserRouter(
         {
           path: '/sign-in',
           element: <SignIn />,
+        },
+        {
+          path: '/sign-up',
+          element: <SignUp />,
         },
       ],
     },
