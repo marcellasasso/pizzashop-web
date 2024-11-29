82ac5034b14d0f70d24218e73291e15fa827bea2
Marcella <marcellasasso@gmail.com>
2024-11-29 15:28:06 -0300

    conexão com API - até aula autenticação do usuário

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import './index.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
 import { router } from './routes'
 
 export function App() {
@@ -13,12 +15,15 @@ export function App() {
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
         <Helmet titleTemplate="%s | pizza.shop" />
         <Toaster richColors />
-        <RouterProvider
-          router={router}
-          future={{
-            v7_startTransition: true,
-          }}
-        />
+
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider
+            router={router}
+            future={{
+              v7_startTransition: true,
+            }}
+          />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )
